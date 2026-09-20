67d16e3eb0f0eea2c6953c676b7c125cbc57a15a
TaekyoungSeok <teakyoung2003@gmail.com>
2026-09-20 20:23:01 +0900

    .

---
+++ 02_Data_Structure/Doubly_linked_list_1.py
@@ -0,0 +1,51 @@
+class DListNode :
+    def __init__(self, data = None):
+        self.data = data
+        self.llink = None
+        self.rlink = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = DListNode()
+
+        self.head.llink = self.head
+        self.head.rlink = self.head
+
+    def insert(self, before, data):
+        newnode = DListNode(data)
+
+        newnode.llink = before
+        newnode.rlink = before.rlink
+
+        before.rlink.llink = newnode
+        before.rlink = newnode
+
+        return newnode
+
+    def delete(self, removed):
+        if removed == self.head:
+            return
+
+        removed.llink.rlink = removed.rlink
+        removed.rlink.llink = removed.llink
+
+    def print_list(self):
+        p = self.head.rlink
+
+        while p != self.head:
+            print(p.data, end = " ")
+            p = p.rlink
+
+        print()
+
+L = DoublyLinkedList()
+
+n1 = L.insert(L.head, 10)
+n2 = L.insert(n1, 20)
+n3 = L.insert(n2, 30)
+
+L.print_list()
+
+L.delete(n2)
+
+L.print_list()
